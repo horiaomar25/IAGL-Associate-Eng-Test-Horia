d18c325ad970041330414da89bd07d0a6899c3ef
horiaomar25 <horiaomar6@gmail.com>
2024-11-17 17:59:50 +0000

    added: error handling if user tries to submit empty input

--- src/component/addToDo.js
+++ src/component/addToDo.js
@@ -5,27 +5,41 @@ import { addTodo } from "../actions";
 const AddTodo = () => {
   const dispatch = useDispatch();
   const [task, setTask] = useState("");
+  const [error, setError] = useState("");
 
   const handleAddTodo = (e) => {
     e.preventDefault(); // Prevent the default form submission behavior
-    if (task.trim()) {
-      dispatch(addTodo({ task }));
-      setTask("");
+
+    // Validation
+    if (!task.trim()) {
+      setError("Must write something to add a task");
+      return;
     }
+
+    if (task.length < 3) {
+      setError("Task must be at least 3 characters long");
+      return;
+    }
+
+    // Clear error and dispatch action
+    setError("");
+    dispatch(addTodo({ task }));
+    setTask("");
   };
 
   return (
     <div>
-    <form className="form-container" onSubmit={handleAddTodo}>
-      <input
-        type="text"
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
-        placeholder="Add new task"
-        className="add-task-input"
-      />
-      <button type="submit">Add Task</button>
-    </form>
+      <form className="form-container" onSubmit={handleAddTodo}>
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Add new task"
+          className="add-task-input"
+        />
+        <button type="submit">Add Task</button>
+      </form>
+      {error && <p className="error-message">{error}</p>}
     </div>
   );
 };
